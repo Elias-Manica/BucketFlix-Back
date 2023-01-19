43a60a1071947343fe3d5f7e21c4f6e17169c7fd
elias <eliasmanica@poli.ufrj.br>
2023-01-19 13:36:35 -0300

    feat: prisma implemented

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
+import connection from "./database/database.js";
+import { getMovies } from "./repositories/listMovies-repositorie.js";
+
 const server = express();
 
 server.use(cors());
@@ -9,8 +12,9 @@ server.use(express.json());
 
 dotenv.config();
 
-server.get("/status", (req, res) => {
-  res.sendStatus(201);
+server.get("/status", async (req, res) => {
+  const response = await getMovies();
+  res.status(201).send(response);
 });
 
 server.listen(process.env.PORT, () => {
